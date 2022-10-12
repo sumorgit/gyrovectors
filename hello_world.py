9aa33051bc2a699e26166402a70ca48dea512786
Raffaelbdl <raffaelbdl@gmail.com>
2022-10-13 00:22:42 +0900

    :lipstick: Better hello_world

--- hello_world.py
+++ hello_world.py
@@ -16,11 +16,12 @@ def main():
     n_iterations = 100  # number of times the operations are applied
     mult_factor = 1.1  # factor by which we want to multiply
 
-    init_size = 20  # plotting only, initial size of the points
-
     # Initialize plot
     fig, ax = init_circle_figure(radius)
+    legends = ["boundary"]
+
     ax.scatter(0, 0, c="black")
+    legends.append("Center")
 
     # Initialize points
     a = np.array([0.0, 0.5])
@@ -29,25 +30,31 @@ def main():
 
     # Plot a and b
     ax.scatter(a[0], a[1], c="b")
+    legends.append("A")
     ax.scatter(b[0], b[1], c="r")
+    legends.append("B")
 
     # At each iteration, we perform one addition and one multiplication
-    w = mob_add(a, supp, radius, back)
-    ax.scatter(w[0], w[1], c="orange", s=init_size)
+    W = np.empty((n_iterations, 2), np.float32)
+    W[0] = mob_add(a, supp, radius, back)
+    X = np.empty((n_iterations, 2), np.float32)
+    X[0] = mob_multiply(mult_factor, a, radius, back)
 
-    x = mob_multiply(mult_factor, a, radius, back)
-    ax.scatter(x[0], x[1], c="cyan", s=init_size)
+    for i in range(1, n_iterations):
+        W[i] = mob_add(W[i - 1], supp, radius, back)
+        X[i] = mob_multiply(mult_factor, X[i - 1], radius, back)
 
-    for i in range(2, n_iterations + 1):
-        w = mob_add(w, supp, radius, back)
-        ax.scatter(w[0], w[1], c="orange", s=init_size / i)
+    # Plot the additions and multiplications
+    ax.scatter(W[..., 0], W[..., 1], c="orange", s=2)
+    legends.append("Addition")
 
-        x = mob_multiply(mult_factor, x, radius, back)
-        ax.scatter(x[0], x[1], c="cyan", s=init_size / i)
+    ax.scatter(X[..., 0], X[..., 1], c="cyan", s=2)
+    legends.append("Multiplication")
 
     # Plot the gyroline between a and b
     fig, ax = plot_gyroline(a, b, radius, back, 70, fig, ax, color="pink", s=2)
 
+    plt.legend(legends)
     plt.show()
 
 
